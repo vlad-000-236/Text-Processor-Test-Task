f237fa8c81c3c3e77d94c0c3ce3b7fc5c010870f
Vlad Iltekov <102834644+vlad-000-236@users.noreply.github.com>
2025-02-03 07:17:07 +0800

    Выполненное задание.

---
+++ TextProcessor/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TextProcessor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnProcessText = new System.Windows.Forms.Button();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnClearFolder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 39);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(340, 306);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.AcceptsReturn = true;
+            this.txtResult.AcceptsTab = true;
+            this.txtResult.Cursor = System.Windows.Forms.Cursors.UpArrow;
+            this.txtResult.Location = new System.Drawing.Point(448, 39);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(340, 306);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnProcessText
+            // 
+            this.btnProcessText.Location = new System.Drawing.Point(10, 385);
+            this.btnProcessText.Name = "btnProcessText";
+            this.btnProcessText.Size = new System.Drawing.Size(111, 35);
+            this.btnProcessText.TabIndex = 2;
+            this.btnProcessText.Text = "Выполнить преобразование";
+            this.btnProcessText.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(127, 385);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(111, 35);
+            this.btnSelectFolder.TabIndex = 3;
+            this.btnSelectFolder.Text = "Отсортировать файлы";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnClearFolder
+            // 
+            this.btnClearFolder.Location = new System.Drawing.Point(244, 385);
+            this.btnClearFolder.Name = "btnClearFolder";
+            this.btnClearFolder.Size = new System.Drawing.Size(111, 35);
+            this.btnClearFolder.TabIndex = 4;
+            this.btnClearFolder.Text = "Очистить папку";
+            this.btnClearFolder.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClearFolder);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.btnProcessText);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Name = "MainForm";
+            this.Text = "Text Processor Test Task";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnProcessText;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnClearFolder;
+    }
+}
+
---
+++ TextProcessor/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TextProcessor
+{
+    public partial class MainForm : Form
+    {
+
+        private static string filePath = Path.Combine(FoldCreator(), "ProcessedText.txt");
+
+        public MainForm()
+        {
+            InitializeComponent();
+            btnProcessText.Click += new EventHandler(BtnProcessText_Click);
+            btnSelectFolder.Click += new EventHandler(BtnSelectFolder_Click);
+            btnClearFolder.Click += new EventHandler(BtnClearFolder_Click);
+
+        }
+
+        // Создаём папку для вывода ".тхт" файла
+        private static string FoldCreator()
+        {
+            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TextProcessorFiles");
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            return folderPath;
+        }
+
+        private void BtnProcessText_Click(object sender, EventArgs e)
+        {
+
+            string inputText = txtInput.Text;
+
+            // Попытка прочитать дату. Если это не дата, программа попытается обработать эту строку, как стандартный текст
+            if (DateTime.TryParseExact(inputText, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date))
+            {
+                txtResult.Clear();
+                /*
+                txtResult.AppendText($"Дата в формате 'дд-мм-гггг': {date:dd-MM-yyyy}\n");
+                txtResult.AppendText($"День недели: {date.DayOfWeek}\n");
+                txtResult.AppendText($"Разница в днях от текущей даты: {(DateTime.Now - date).Days} дней\n");*/
+
+                txtResult.Text = $"\nДата в формате 'дд-мм-гггг': {date:dd-MM-yyyy}" +
+                                 $"\n День недели: {date.DayOfWeek}" +
+                                 $"\n Разница в днях от текущей даты: {(DateTime.Now - date).Days} дней";
+            }
+            else
+            {
+                txtResult.Clear();
+                MessageBox.Show("Невозможно распознать дату. Данный текст будет обработан, как стандартная строка.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Удаление знаков припенания
+                string punctuationReplacer = Regex.Replace(inputText, "[-.?!)(,:]", "");
+
+                // Делаем первую букву каждого слова заглавной
+                string capitalizedText = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(punctuationReplacer.ToLower());
+
+                // Поменяем местами первую и последнюю букву в каждом слове
+                string swappedText = string.Join(" ", capitalizedText.Split(' ').Select(SwapFirstLastChar));
+
+                // Удаление пробелов и переносов строк
+                string trimmedText = Regex.Replace(swappedText, @"\s+", "");
+
+                // Получаем массив слов без знаков препинания и выводим в алфавитном порядке
+                var words = trimmedText.Split(new[] { ' ', '.', ',', '!', '?', ';', ':', '"' }, StringSplitOptions.RemoveEmptyEntries)
+                                        .Distinct()
+                                        .OrderBy(w => w)
+                                        .ToArray();
+
+                // Сохраняем текст в файл
+                for (int a = 0; a < words.Length; a++)
+                {
+                    File.WriteAllText(filePath, words[a]);
+                }
+                txtResult.Text = $"Файл сохранен по пути: {filePath}";
+            }
+
+
+            /*
+            // Обработка даты
+            if (DateTime.TryParseExact(txtDate.Text, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date))
+            {
+                txtResult.AppendText($"\nДата в формате 'дд-мм-гггг': {date:dd-MM-yyyy}");
+                txtResult.AppendText($"\nДень недели: {date.DayOfWeek}");
+                txtResult.AppendText($"\nРазница в днях от текущей даты: {(DateTime.Now - date).Days} дней");
+            }
+            else
+            {
+                MessageBox.Show("Невозможно распознать дату. Пожалуйста, введите дату в формате дд.мм.гггг.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+        }
+
+        private string SwapFirstLastChar(string word)
+        {
+            if (word.Length <= 1)
+                return word;
+
+            char[] charArray = word.ToCharArray();
+            charArray[0] = word[word.Length - 1];
+            charArray[word.Length - 1] = word[0];
+            return new string(charArray);
+        }
+
+        private void BtnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderDialog.SelectedPath;
+                    OrganizeFilesByExtension(selectedPath);
+                }
+            }
+        }
+
+        private void OrganizeFilesByExtension(string path)
+        {
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                try
+                {
+                    string extension = Path.GetExtension(file).TrimStart('.').ToLower();
+                    string directoryPath = Path.Combine(path, extension);
+
+                    if (!Directory.Exists(directoryPath))
+                    {
+                        Directory.CreateDirectory(directoryPath);
+                    }
+
+                    string newFilePath = Path.Combine(directoryPath, Path.GetFileName(file));
+                    File.Move(file, newFilePath);
+                    txtResult.Text = "Ваши файлы отсортированы по новым папкам.";
+                }
+                catch (Exception ex)
+                {
+                    // Игнорируем ошибки и продолжаем
+                    Console.WriteLine($"Ошибка при перемещении {file}: {ex.Message}");
+                }
+            }
+        }
+
+        private void BtnClearFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderDialog.SelectedPath;
+                    ClearFolder(selectedPath);
+                }
+            }
+        }
+
+        private void ClearFolder(string path)
+        {
+            try
+            {
+                // Удаляем все файлы в папке
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    File.Delete(file);
+                }
+
+                // Удаляем все подпапки в папке
+                foreach (string dir in Directory.GetDirectories(path))
+                {
+                    Directory.Delete(dir, true);
+                }
+                txtResult.Text = ("Содержимое папки успешно удалено.");
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = ($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+}
